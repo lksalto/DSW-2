5e150eefa08003e55ad159462cdb2a1e96a64f79
lksalto <saltolks@gmail.com>
2024-12-15 21:36:03 -0300

    ajustes arquivos

--- marketplace/react/aplicacaoCompleta/worldcup-pool-frontend/src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-export function formatDate(date) {
-    const dateObj = new Date(date);
-    dateObj.setTime( dateObj.getTime() + dateObj.getTimezoneOffset()*60*1000 );
-    return dateObj.toLocaleDateString();
-}
-
